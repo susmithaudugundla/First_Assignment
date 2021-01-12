2a0f5e4c959bc086a7dcba2a05477575c2ccb85d
Susmitha <udugundlasusmitha@gmail.com>
2021-01-12 12:13:17 +0530

    mongodb atlas used

--- routes/apis/students.js
+++ routes/apis/students.js
@@ -1,46 +1,63 @@
 const express = require('express');
 const router = express.Router();
 
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'rgukt123',
-    database:'students'
-})
-connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-});
+const MongoClient = require('mongodb').MongoClient;
+const schema = require('mongodb-schema');
+let database;
+const uri = "mongodb+srv://FirstAssignment:Susmi@123@assignment-1.ksf6u.mongodb.net/Students?retryWrites=true&w=majority";
+MongoClient.connect(uri,{ useUnifiedTopology: true, useNewUrlParser: true }, (err, conn) => {
+        if (err) {
+            console.log("Connection failed to database", err);
+          } else {
+            console.log("Connection Successfull");
+            database = conn.db("Students");
+          }
+        }
+  )
+
 
 // Get all students
-router.get('/', (req, res) => {
-    connection.query('SELECT * from student_details', function (err, rows, fields) {
-        if (err) throw err
-        console.log('The solution is: ', rows);
-        //res.status(200).json({msg:"success"});
-        res.render('students', {rows});
-    })  
-})
+router.get('/', async (req, res) => {
+            try{
+                let allStudents = await database.collection("students_data").find({}).toArray();
+                //console.log(allTasks);
+                res.render('students', {allStudents});
+            }
+            catch (err){
+                console.error(err);
+            }
+          } 
+)
 
 // Create a student record
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     if(!req.body.id || !req.body.name || !req.body.branch || !req.body.email || !req.body.password){
         res.status(400).json({msg:"You have to enter all the details"})
     }
     else{
-        const stmt = `insert into student_details(id, name,branch, email, password )values(?,?,?,?,?)`;
-        const val = [req.body.id, req.body.name, req.body.branch, req.body.email, req.body.password];
-        connection.query(stmt,val,(err, results, fields) => {
-            if (err) {
-              return console.error(err.message);
-            }
-            // get inserted id
+        try{
+            let addStudent = await database.collection("students_data").insertOne({
+                id:req.body.id,
+                name: req.body.name,
+                email:req.body.email,
+                branch:req.body.branch,
+                password:req.body.password
+            });
             res.redirect('/');
-        })  
+        }
+        catch(err){
+            console.error(err);
+        }
+        // const stmt = `insert into student_details(id, name,branch, email, password )values(?,?,?,?,?)`;
+        // const val = [req.body.id, req.body.name, req.body.branch, req.body.email, req.body.password];
+        // connection.query(stmt,val,(err, results, fields) => {
+        //     if (err) {
+        //       return console.error(err.message);
+        //     }
+        //     // get inserted id
+        //     res.redirect('/');
+        // })  
     }
 })
 
--- routes/isstudent.js
+++ routes/isstudent.js
@@ -1,34 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'rgukt123',
-    database:'students'
-})
-connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-});
-router.post('/', (req, res) => {
+const MongoClient = require('mongodb').MongoClient;
+const schema = require('mongodb-schema');
+let database;
+const uri = "mongodb+srv://FirstAssignment:Susmi@123@assignment-1.ksf6u.mongodb.net/Students?retryWrites=true&w=majority";
+MongoClient.connect(uri,{ useUnifiedTopology: true, useNewUrlParser: true }, (err, conn) => {
+        if (err) {
+            console.log("Connection failed to database", err);
+          } else {
+            console.log("Connection Successfull");
+            database = conn.db("Students");
+          }
+        }
+  )
+
+router.post('/', async (req, res) => {
     if(!req.body.id || !req.body.password){
         res.status(400).json({msg:"You have to enter all the details"})
     }
     else{
-        connection.query('SELECT * from student_details', function (err, rows, fields) {
-            if (err) throw err
-            const found = rows.some( row => row.id == req.body.id && row.password == req.body.password );
+        try{
+            let allStudents = await database.collection("students_data").find({}).toArray();
+            const found = allStudents.some( row => row.id == req.body.id && row.password == req.body.password );
             if(found){
                 res.send("<h3>Logged in successfully</h3>");
             }
             else{
                 res.send("<h3>Entered incorrect details</h3>");
             }
-        })
+        }
+        catch(err){
+            console.log(err);
+        }
     }
 })
 
